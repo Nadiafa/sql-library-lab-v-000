3992e66c33965e2ce111159f08c4209a93daa8ac
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 13:27:26 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,23 +1,27 @@
-series table
-    creates a series table with a title field (FAILED - 1)
-    creates a series table with a author_id field (FAILED - 2)
-    creates a series table with a subgenre_id field (FAILED - 3)
-    creates a series table with an id as a primary key (FAILED - 4)
-  subgenres table
-    creates a subgenres table with a name field (FAILED - 5)
-    creates a subgenres table with an id as a primary key (FAILED - 6)
-  authors table
-    creates a authors table with a name field (FAILED - 7)
-    creates a authors table with an id as a primary key (FAILED - 8)
-  books table
-    creates a books table with a title field (FAILED - 9)
-    creates a books table with a year field (FAILED - 10)
-    creates a books table with a series_id field (FAILED - 11)
-    creates a books table with an id as a primary key (FAILED - 12)
-  characters table
-    creates a characters table with a name field (FAILED - 13)
-    creates a characters table with a species field (FAILED - 14)
-    creates a characters table with a motto field (FAILED - 15)
-    creates a characters table with a series_id field (FAILED - 16)
-    creates a characters table with a author_id field (FAILED - 17)
-    creates a characters table with an id as a primary key (FAILED - 18)
+CREATE TABLE series (
+    title
+    author_id
+    subgenre_id
+    id as a primary key
+CREATE TABLE subgenres (
+    a name
+    a id as a primary key
+CREATE TABLE authors (
+    name
+    id as a primary key
+CREATE TABLE books (
+    title
+    year 
+    series_id 
+    id as a primary key 
+CREATE TABLE characters (
+    name 
+    species 
+    motto 
+    series_id 
+    author_id 
+    id as a primary key 
+CREATE TABLE character_books ( 
+    id as a primary key 
+    book_id 
+    character_id 
