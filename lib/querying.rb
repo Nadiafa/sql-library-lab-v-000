223ff4b6ecac37895bb6b166f6d2de353dd1ebb0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 14:32:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,10 +24,13 @@ JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species) FROM series
-JOIN characters ON series.id = characters.series_id
+  "SELECT series.title FROM series
+JOIN books ON series.id = books.series_id
+JOIN character_books ON books.id = character_books.book_id
+JOIN characters ON character_books.character_id = characters.id
 WHERE characters.species = 'human'
-ORDER BY COUNT(characters.species) DESC 
+GROUP BY series.title
+ORDER BY COUNT(*) DESC
 LIMIT 1;"
 end
 
