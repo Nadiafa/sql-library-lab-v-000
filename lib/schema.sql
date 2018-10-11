817fb3995bba47571a5bdbc89b215fd569848326
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 13:24:29 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,16 @@
+series table
+    creates a series table with a title field (FAILED - 1)
+    creates a series table with a author_id field (FAILED - 2)
+    creates a series table with a subgenre_id field (FAILED - 3)
+    creates a series table with an id as a primary key (FAILED - 4)
+  subgenres table
+    creates a subgenres table with a name field (FAILED - 5)
+    creates a subgenres table with an id as a primary key (FAILED - 6)
+  authors table
+    creates a authors table with a name field (FAILED - 7)
+    creates a authors table with an id as a primary key (FAILED - 8)
+  books table
+    creates a books table with a title field (FAILED - 9)
+    creates a books table with a year field (FAILED - 10)
+    creates a books table with a series_id field (FAILED - 11)
+    creates a books table with an id as a primary key (FAILED - 12)
